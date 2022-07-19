8035d4a63bacac14f2d257e7f9d2e2ddfc1cb7df
0jjjino <109055414+0jjjino@users.noreply.github.com>
2022-07-19 13:53:33 +0900

    FRONT-452: Header 만들기

---
+++ src/components/HeaderContainer.tsx
@@ -0,0 +1,51 @@
+import styled from "@emotion/styled";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import backIcon from "../image/backIcon.svg";
+import basket from "../image/basket.svg";
+import FixedContainer from "./FixedContainer";
+import Icon from "./Icon";
+
+function Header() {
+	const navigate = useNavigate();
+
+	const handleBackClick = () => {
+		navigate(-1);
+	};
+
+	return (
+		<FixedContainer height={56}>
+			<InnerContainer>
+				<Container>
+					<Icon src={backIcon} onIconClick={handleBackClick} />
+					<Title>이벤트</Title>
+				</Container>
+				<Icon src={basket} />
+			</InnerContainer>
+		</FixedContainer>
+	);
+}
+
+export default Header;
+
+const InnerContainer = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding: 14px 16px 14px 24px;
+`;
+
+const Container = styled.div`
+	display: flex;
+	justify-content: space-around;
+`;
+
+const Title = styled.div`
+	height: 28px;
+	margin-left: 40px;
+	font-weight: 400;
+	font-size: 19px;
+	font-family: "Open Sans", sans-serif;
+	line-height: 28px;
+`;
